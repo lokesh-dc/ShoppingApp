af295cd0151ce0cf16771e838e4a81455b8b93eb
lokesh-dc <lokesh.cdewanand@gmail.com>
2022-12-15 14:12:04 +0530

    Server

--- Config/dbConnect.js
+++ Config/dbConnect.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const dbConnect = () =>{
+    mongoose.set("strictQuery", false);
     return mongoose.connect("mongodb+srv://lokesh:Prashant@shoppingdatabase.pn7wkbm.mongodb.net/database");
 }
 
